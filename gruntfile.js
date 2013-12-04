d012bff7b795173a5b9348771a4bce4a88aa03f9
james2doyle <james2doyle@gmail.com>
2013-12-04 11:03:23 -0500

    add gruntfile and package json

---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*! <%= pkg.name %> - <%= pkg.description %> \\n' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;\\n' +
+      '* Released under <%= _.pluck(pkg.licenses, "type").join(", ") %> license */'
+    },
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      ie: {
+        files: {
+          'ki.ie8.min.js': ['ki.ie8.js']
+        }
+      },
+      normal: {
+        files: {
+          'ki.min.js': ['ki.js']
+        }
+      },
+      all: {
+        files: {
+          'ki.all.min.js': ['ki.js', 'ki.ie8.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify:normal']);
+};
