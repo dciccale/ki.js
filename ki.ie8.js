007e016a4217dbb2775547f33f32c06a3680fbcf
james2doyle <james2doyle@gmail.com>
2014-02-03 13:57:08 -0500

    formatting and trailing comma

--- ki.ie8.js
+++ ki.ie8.js
@@ -3,8 +3,9 @@
  * Copyright (c) 2013 Denis Ciccale (@tdecs)
  * Released under MIT license
  */
-!function(a, b, c, d, e, f) {
-    /*
+! function(a, b, c, d, e, f) {
+
+  /*
    * (internal use)
    * ie8 workaround to convert NodeList to an array
    * a = NodeList
@@ -13,10 +14,12 @@
    * d = empty array
    * returns array of dom nodes
    */
-    e = function(a, b, c, d) {
-        for (c = -1, d = []; b = a[++c]; ) d[c] = b;
-        return d;
-    }, /*
+  e = function(a, b, c, d) {
+    for (c = -1, d = []; b = a[++c];) d[c] = b;
+    return d;
+  },
+
+  /*
    * (internal use)
    * Cross-browser super-type event handler https://gist.github.com/dciccale/5521816
    * action = 'on' or 'off'
@@ -25,85 +28,95 @@
    * callback = function to execute when event is triggered
    * method = placeholder for the native method to call (internal use)
    */
-    f = function(a, b, c, d, e) {
-        e = {
-            on: "addEventListener",
-            off: "removeEventListener"
-        }[a];
-        try {
-            c[e](b, d, !1);
-        } catch (f) {
-            e = {
-                on: "attachEvent",
-                off: "detachEvent"
-            }[a], c[e](a + b, function() {
-                d.apply(c, arguments);
-            });
-        }
-    }, /*
+  f = function(a, b, c, d, e) {
+    e = {
+      on: "addEventListener",
+      off: "removeEventListener"
+    }[a];
+    try {
+      c[e](b, d, !1);
+    } catch (f) {
+      e = {
+        on: "attachEvent",
+        off: "detachEvent"
+      }[a], c[e](a + b, function() {
+        d.apply(c, arguments);
+      });
+    }
+  },
+
+  /*
    * $ main method
    * a = css selector, dom object, or function
    * returns instance
    */
-    this.$ = function(a) {
-        return new $[c].i(a);
-    }, // ki prototype
-    d = {
-        // default length
-        length: 0,
-        /*
+  this.$ = function(a) {
+    return new $[c].i(a);
+  }, // ki prototype
+  d = {
+    // default length
+    length: 0,
+
+    /*
      * init method (internal use)
      * a = selector, dom element or function
      */
-        i: function(c) {
-            b.push.apply(this, c && c.nodeType ? [ c ] : "" + c === c ? e(a.querySelectorAll(c)) : /^f/.test(typeof c) ? $(a).r(c) : null);
-        },
-        /*
+    i: function(c) {
+      b.push.apply(this, c && c.nodeType ? [c] : "" + c === c ? e(a.querySelectorAll(c)) : /^f/.test(typeof c) ? $(a).r(c) : null);
+    },
+
+    /*
      * ready method
      * Smallest DOMReady code, ever
      * http://www.dustindiaz.com/smallest-domready-ever
      * a = function to call when dom is ready
      * return this
      */
-        r: function(b) {
-            return /c/.test(a.readyState) ? b() : $(a).on("DOMContentLoaded", b), this;
-        },
-        /*
+    r: function(b) {
+      return /c/.test(a.readyState) ? b() : $(a).on("DOMContentLoaded", b), this;
+    },
+
+    /*
      * on method
      * a = string event type i.e 'click'
      * b = function
      * return this
      */
-        on: function(a, b) {
-            return this.each(function() {
-                f("on", a, this, b);
-            });
-        },
-        /*
+    on: function(a, b) {
+      return this.each(function() {
+        f("on", a, this, b);
+      });
+    },
+
+    /*
      * off method
      * a = string event type i.e 'click'
      * b = function
      * return this
      */
-        off: function(a, b) {
-            return this.each(function() {
-                f("off", a, this, b);
-            });
-        },
-        /*
+    off: function(a, b) {
+      return this.each(function() {
+        f("off", a, this, b);
+      });
+    },
+
+    /*
      * each method
      * use native forEach to iterate collection
      * a = the function to call each loop
      * (b = internal use)
      * return this
      */
-        each: function(a, b, c, d) {
-            for (b = this, c = 0, d = b.length; d > c; ++c) a.call(b[c], b[c], c, b);
-            return b;
-        },
-        // for some reason is needed to get an array-like
-        // representation instead of an object
-        splice: b.splice
-    }, // set prototypes
-    $[c] = d.i[c] = d;
+    each: function(a, b, c, d) {
+      for (b = this, c = 0, d = b.length; d > c; ++c) a.call(b[c], b[c], c, b);
+      return b;
+    }
+
+    // for some reason is needed to get an array-like
+    // representation instead of an object
+    splice: b.splice
+  }
+
+  // set prototypes
+  $[c] = d.i[c] = d;
 }(document, [], "prototype");
